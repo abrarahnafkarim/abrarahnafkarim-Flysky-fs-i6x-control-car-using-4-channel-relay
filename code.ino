6ca63eb021cc1f8ddec82c47d1832863531dd5c0
Abrar Ahnaf Karim <76809336+abrarahnafkarim@users.noreply.github.com>
2024-05-25 17:00:36 +0600

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,10 +1,10 @@
-int enA = 5;
-int in1 = 2;
-int in2 = 3;
-int enB = 6;
-int in3 = 4;
-int in4 = 7;
+// Define pins for relay control
+#define RELAY1_PIN 6 // Left front motor forward
+#define RELAY2_PIN 7 // Left rear motor forward
+#define RELAY3_PIN 8 // Right front motor forward
+#define RELAY4_PIN 9 // Right rear motor forward
 
+// Define receiver pins
 int receiver_pins[] = {A0, A1, A2, A3, A4, A5};
 int receiver_values[] = {0, 0, 0, 0, 0, 0};
 int res_min = 950;
@@ -15,18 +15,17 @@ int working_range = 255; // motor driver range
 boolean prt = true;
 
 int mode = 0;
-//-1 = transmitter not connected or out of range
+// -1 = transmitter not connected or out of range
 // 0 = transmitter connected and ready
 // 1 = slow speed mode
 // 2 = high speed mode
 
 void setup() {
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  // Set relay pins as output
+  pinMode(RELAY1_PIN, OUTPUT);
+  pinMode(RELAY2_PIN, OUTPUT);
+  pinMode(RELAY3_PIN, OUTPUT);
+  pinMode(RELAY4_PIN, OUTPUT);
 
   Serial.begin(115200);
 }
@@ -40,25 +39,24 @@ void loop() {
   int rot = receiver_values[0];
 
   if (mode == 1) {
-    m1 = receiver_values[1] / 2 + (rot)/1.5;
-    m2 = receiver_values[1] / 2 - (rot)/1.5;
+    m1 = receiver_values[1] / 2 + (rot) / 1.5;
+    m2 = receiver_values[1] / 2 - (rot) / 1.5;
 
   } else if (mode == 2) {
-
     m1 = receiver_values[1] + rot / 1.75;
     m2 = receiver_values[1] - rot / 1.75;
   }
 
-  mpower(1,  m1);
-  mpower(2,  m2);
+  // Control relays based on motor values
+  controlRelays(m1, m2);
 }
 
 int rp = 0;
 
 void receive() {
-  receiver_values[rp] = map(pulseIn (receiver_pins[rp], HIGH), res_min, res_max, -1 * working_range, working_range);
+  receiver_values[rp] = map(pulseIn(receiver_pins[rp], HIGH), res_min, res_max, -1 * working_range, working_range);
   rp++;
-  if (rp == 6){
+  if (rp == 6) {
     rp = 0;
   }
   boolean activevalues = true;
@@ -87,40 +85,36 @@ void receive() {
   }
 }
 
-void mpower(int motor,  int spd) {
-  int rotation = 0;
-  if (spd > 0) {
-    rotation = 1;
-  } else if (spd < 0) {
-    rotation = -1;
-    spd *= -1;
-  }
-  if (spd > 255) {
-    spd = 255;
-  }
-  int pwm;
-  int pA;
-  int pB;
-  if (motor == 1) {
-    pwm = enA;
-    pA = in1;
-    pB = in2;
-  } else if (motor == 2) {
-    pwm = enB;
-    pA = in3;
-    pB = in4;
+void controlRelays(int m1, int m2) {
+  if (m1 > 0 && m2 > 0) {
+    // Forward
+    digitalWrite(RELAY1_PIN, HIGH); // Left front motor forward
+    digitalWrite(RELAY2_PIN, HIGH); // Left rear motor forward
+    digitalWrite(RELAY3_PIN, HIGH); // Right front motor forward
+    digitalWrite(RELAY4_PIN, HIGH); // Right rear motor forward
+  } else if (m1 < 0 && m2 < 0) {
+    // Backward
+    digitalWrite(RELAY1_PIN, LOW); // Left front motor backward
+    digitalWrite(RELAY2_PIN, LOW); // Left rear motor backward
+    digitalWrite(RELAY3_PIN, LOW); // Right front motor backward
+    digitalWrite(RELAY4_PIN, LOW); // Right rear motor backward
+  } else if (m1 > 0 && m2 < 0) {
+    // Left turn
+    digitalWrite(RELAY1_PIN, LOW); // Left front motor backward
+    digitalWrite(RELAY2_PIN, LOW); // Left rear motor backward
+    digitalWrite(RELAY3_PIN, HIGH); // Right front motor forward
+    digitalWrite(RELAY4_PIN, HIGH); // Right rear motor forward
+  } else if (m1 < 0 && m2 > 0) {
+    // Right turn
+    digitalWrite(RELAY1_PIN, HIGH); // Left front motor forward
+    digitalWrite(RELAY2_PIN, HIGH); // Left rear motor forward
+    digitalWrite(RELAY3_PIN, LOW); // Right front motor backward
+    digitalWrite(RELAY4_PIN, LOW); // Right rear motor backward
   } else {
-    return;
+    // Stop
+    digitalWrite(RELAY1_PIN, LOW); // Stop left front motor
+    digitalWrite(RELAY2_PIN, LOW); // Stop left rear motor
+    digitalWrite(RELAY3_PIN, LOW); // Stop right front motor
+    digitalWrite(RELAY4_PIN, LOW); // Stop right rear motor
   }
-  if (rotation == 0) {
-    digitalWrite(pA, LOW);
-    digitalWrite(pB, LOW);
-  } else if (rotation == 1) {
-    digitalWrite(pA, HIGH);
-    digitalWrite(pB, LOW);
-  } else if (rotation == -1) {
-    digitalWrite(pA, LOW);
-    digitalWrite(pB, HIGH);
-  }
-  analogWrite(pwm, spd);
 }
